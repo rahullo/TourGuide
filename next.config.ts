2197e92f82be39a0b16c480ed2b09019de763cf4
Rahul Lohra <rahullohra987@gmail.com>
2026-08-11 15:21:11 +0530

    Fix NEXTAUTH_URL build error on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || `https://${process.env.VERCEL_URL}` || 'http://localhost:3000',
+  }
 };
 
 export default nextConfig;
